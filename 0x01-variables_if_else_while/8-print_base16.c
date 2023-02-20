fe81a36ebe586fbb51aa1b7db05b0cce5b70705c
1Sine <snashskosana@yahoo.com>
2023-02-20 10:40:49 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,21 +3,21 @@
 #include <time.h>
 
 /**
- * Description - Numbers in lower case
- * main -Program that prints numbers in lowercase
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
 	int i;
 	char low;
+
 	for (i = '0'; i <= '9'; i++);
 	putchar(i);
-	
 	for (low = 'a'; low <= 'f'; low++);
 	putchar(low);
 	putchar('\n');
-	
+
 	return (0);
 }
